0545010494c2b870afdc7725f004c9768f289c1e
Mike <mikestewart729@gmail.com>
2022-02-08 14:32:47 -0500

    Set up homepage views

--- podcasts/views.py
+++ podcasts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+# podcasts/views.py
 
-# Create your views here.
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Episode
+
+class HomePageView(ListView):
+    template_name = "homepage.html"
+    model = Episode
+
+    def get_context(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["episodes"] = Episode.objects.filter().order_by("-pub_date")[:10]
+        return context
