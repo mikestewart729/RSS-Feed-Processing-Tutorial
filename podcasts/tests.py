cc39ec00dfae496998c12cb3de21327f5e13d49c
Mike <mikestewart729@gmail.com>
2022-02-08 09:36:26 -0500

    Create Episode model

--- podcasts/tests.py
+++ podcasts/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+# podcasts/tests.py
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+from .models import Episode
+
+class PodcastsTests(TestCase):
+    def setUp(self):
+        self.episode = Episode.objects.create(
+            title="My Awesome Podcast Episode",
+            description="Look, I made a podcast!",
+            pub_date=timezone.now(),
+            link="https://myawesomeshow.com",
+            image="https://image.myawesomeshow.com",
+            podcast_name="My Python Podcast",
+            guid="de194720-7b4c-49e2-a05f-432436d3fetr",
+        )
+
+    def test_episode_content(self):
+        self.assertEqual(self.episode.description, "Look, I made a podcast!")
+        self.assertEqual(self.episode.link, "https://myawesomeshow.com")
+        self.assertEqual(
+            self.episode.guid, "de194720-7b4c-49e2-a05f-432436d3fetr"
+        )
+
+    def test_episode_str_representation(self):
+        self.assertEqual(
+            str(self.episode), "My Python Podcast: My Awesome Podcast Episode"
+        )
